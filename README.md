f0e9ae8d68985b6a76ec7c9783342a39ae180449
Scott Hsieh <scott@one45.com>
2018-09-20 11:39:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# chartjs-plugin-whisker
+ChartJS plugin that allows you to draw error bar whiskers onto your existing chart
---
+++ src/plugin.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const plugin = {
+  id: 'sort',
+  options: [],
+  beforeInit: function (chart) {
+  },
+  afterInit: function (chart) {
+
+  }
+}
+
+export default plugin
