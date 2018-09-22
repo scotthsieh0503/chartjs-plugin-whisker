4b5699538a3775b1198b1f0f18fa0747d3649028
Scott Hsieh <scott@one45.com>
2018-09-21 19:30:15 -0700

    partial commit for drawing labels

--- src/plugin.js
+++ src/plugin.js
@@ -7,7 +7,6 @@ const plugin = {
   beforeInit: function (chart) {
     this.options = chart.chart.config.options.plugins.whiskers
   },
-
   afterDraw: function (chart) {
     this.drawWhiskers(chart)
     this.drawingLegend(chart)
@@ -18,6 +17,7 @@ const plugin = {
     let datasets = chart.config.data.datasets
     let options = this.options
     let xAxis = chart.scales['x-axis-0']
+    let _this = this
 
     datasets.forEach(function (element, index, array) {
       if (element.whiskers) {
@@ -26,42 +26,13 @@ const plugin = {
         element.data.forEach(function (element, index, array) {
           let metaData = chart.getDatasetMeta(datasetIndex)
           let barThickness = metaData.data[index]._view.height
-
-          // drawing main error lines
           let centerY = metaData.data[index]._view.y
           let minX = xAxis.getPixelForValue(whiskers[index].min)
           let maxX = xAxis.getPixelForValue(whiskers[index].max)
-          ctx.beginPath()
-          ctx.lineWidth = options.lineWidth
-          ctx.strokeStyle = options.color
-          ctx.moveTo(minX, centerY)
-          ctx.lineTo(maxX, centerY)
-          ctx.stroke()
-
-          // drawing min-whiskers
-          ctx.beginPath()
-          ctx.lineWidth = options.lineWidth
-          ctx.strokeStyle = options.color
-          ctx.moveTo(minX, centerY + barThickness / 5)
-          ctx.lineTo(minX, centerY - barThickness / 5)
-          ctx.stroke()
-
-          // drawing max-whsikers
-          ctx.beginPath()
-          ctx.lineWidth = options.lineWidth
-          ctx.strokeStyle = options.color
-          ctx.moveTo(maxX, centerY + barThickness / 5)
-          ctx.lineTo(maxX, centerY - barThickness / 5)
-          ctx.stroke()
-
-          // drawing circles
-          ctx.beginPath()
           let centerX = xAxis.getPixelForValue(whiskers[index].mean)
-          ctx.arc(centerX, centerY, options.lineWidth, 0, 2 * Math.PI)
-          ctx.fillStyle = options.color
-          ctx.strokeStyle = options.color
-          ctx.stroke()
-          ctx.fill()
+
+          // Drawing drawWhiskers
+          _this.drawWhisker(ctx, minX, maxX, centerX, centerY, barThickness / 5)
 
           // drawing average labels
           let labelX = maxX + 10
@@ -77,18 +48,60 @@ const plugin = {
   },
   drawingLegend: function (chart) {
     let ctx = chart.ctx
-    let options = this.options
     let legend = chart.legend
-    let legendLeft = legend.left
-    let legendWidth = legend.width
+    let legendLeft = legend.left + 10 // add 10px padding to the left
+    let legendWidth = legend.width * 0.8
     let legendElementHeight = legend.legendHitBoxes[0].height
     let legendTop = 0
-    legend.legendHitBoxes.forEach(function (element, index, array) {
-      legendTop = legendTop + element.top
-    })
-  },
-  drawWhisker: function (minX, maxX, y, thickness) {
 
+    // calculate position of the whisker on legend
+    legendTop = legend.legendHitBoxes[legend.legendHitBoxes.length - 1].top + 3 * legendElementHeight
+    this.drawWhisker(ctx, legendLeft, legendLeft + legendWidth, legendLeft + legendWidth / 2, legendTop, legendElementHeight / 2)
+
+    // draw legend labels
+    let labelY = legendTop + 2 * legendElementHeight
+    ctx.beginPath()
+    ctx.textBaseline = 'middle'
+    ctx.textAlign = 'center'
+    ctx.font = legendElementHeight * 0.75 + 'px ' + this.options.fontFamily
+    ctx.fillText('Average', legendLeft + legendWidth / 2, labelY)
+    ctx.fillText('-1 STD', legendLeft, labelY)
+    ctx.fillText('+1 STD', legendLeft + legendWidth, labelY)
+    ctx.stroke()
+    ctx.fill()
+  },
+  drawWhisker: function (ctx, minX, maxX, centerX, y, thickness) {
+    // drawing main error lines
+    ctx.beginPath()
+    ctx.lineWidth = this.options.lineWidth
+    ctx.strokeStyle = this.options.color
+    ctx.moveTo(minX, y)
+    ctx.lineTo(maxX, y)
+    ctx.stroke()
+
+    // drawing min-whiskers
+    ctx.beginPath()
+    ctx.lineWidth = this.options.lineWidth
+    ctx.strokeStyle = this.options.color
+    ctx.moveTo(minX, y - thickness)
+    ctx.lineTo(minX, y + thickness)
+    ctx.stroke()
+
+    // drawing max-whsikers
+    ctx.beginPath()
+    ctx.lineWidth = this.options.lineWidth
+    ctx.strokeStyle = this.options.color
+    ctx.moveTo(maxX, y + thickness)
+    ctx.lineTo(maxX, y - thickness)
+    ctx.stroke()
+
+    // drawing circles
+    ctx.beginPath()
+    ctx.arc(centerX, y, this.options.lineWidth, 0, 2 * Math.PI)
+    ctx.fillStyle = this.options.color
+    ctx.strokeStyle = this.options.color
+    ctx.stroke()
+    ctx.fill()
   }
 }
 
